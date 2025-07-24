b78556a5d4d9167337079d0db0246324943e5846
Ahaququq <83945944+ahaququq@users.noreply.github.com>
2025-07-24 18:57:00 +0200

    Change shader compilation to operate on slang

--- compileShaders.sh
+++ compileShaders.sh
@@ -1,8 +1,12 @@
 set -e
 echo "Compiling shaders..."
 
-for f in "$1"/src/shaders/*.????; do
-  name="${f#"$1"/src/shaders}"
-  glslc "$f" -o ./"$name".spv
+for f in "$1"/src/shaders/*.slang; do
+  name="${f##"$1"/src/shaders}"
+  name="${name%%.slang}"
+  name="$name.spv"
+  #glslc "$f" -o ./"$name".spv
+  slangc "$f" -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o ./"$name"
+  echo "$f" "->" "$name"
 done
 echo "Shaders compiled!"
